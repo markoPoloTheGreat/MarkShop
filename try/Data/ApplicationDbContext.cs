5e4409caae9617c8e5e9b875f7d1ad3bb952fc52
User <User@WX1129718>
2025-10-22 16:50:53 +0300

    Add project files.

---
+++ try/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MarkShop.Models;
+namespace MarkShop.Data
+{
+    namespace ShopSbS.Data
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            // Add DbSet<TEntity> properties here, for example:
+            // public DbSet<YourEntity> YourEntities { get; set; }
+        }
+    }
+}
