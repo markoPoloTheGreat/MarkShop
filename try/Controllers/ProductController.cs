a1c696ecd3a10c7acbe78d2bb0a2c054cd24e8cf
User <User@WX1129718>
2025-11-10 14:41:14 +0200

    j

--- try/Controllers/ProductController.cs
+++ try/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MarkShop.Data.ShopSbS.Data;
+﻿using MarkShop.Data.ShopSbS.Data;
+using MarkShop.Models;
+using Microsoft.AspNetCore.Mvc;
 namespace MarkShop.Controllers
 {
     public class ProductController : Controller
@@ -15,6 +16,30 @@ namespace MarkShop.Controllers
         {
             var products = _context.Products.ToList();
             return View(products);
+        }
+        public IActionResult CreatePr()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreatePr(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // If the user entered an image name, prepend /images/
+                if (!string.IsNullOrWhiteSpace(product.ImageUrl) && !product.ImageUrl.StartsWith("/images/"))
+                {
+                    product.ImageUrl = "/images/" + product.ImageUrl.TrimStart('/');
+                }
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(IndexPr1));
+            }
+            return View(product);
+        }
+        public IActionResult EditPr(int id)
+        {
+            return View(id);
         }
     }
 }
